aaf144a7a48d789a5ddf2b28d397783a6a65428c
Konstantin.L <llaskot@gmail.com>
2025-05-31 23:47:52 +0300

    final main

---
+++ main.py
@@ -0,0 +1,5 @@
+import flet as ft
+from ui.main_window import main as start
+
+if __name__ == '__main__':
+    ft.app(target=start)
--- ui/main_window.py
+++ ui/main_window.py
@@ -139,5 +139,5 @@ def main(page: ft.Page):
     page.add(control)
 
 
-if __name__ == "__main__":
-    ft.app(target=main)
+# if __name__ == "__main__":
+#     ft.app(target=main)
